3abf1957b31e7805f51f87d55ac8ca744cd14e9e
Phạm Minh Thịnh <thinhpm>
2019-03-17 15:11:28 +0700

    update

--- main.py
+++ main.py
@@ -293,6 +293,7 @@ def remove_special_characters(string):
     string = string.replace('\r', '')
     string = string.replace('[', '')
     string = string.replace(']', '')
+    string = string.replace('|', '')
 
     return string
 
